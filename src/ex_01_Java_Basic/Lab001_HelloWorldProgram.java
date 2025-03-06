f6c178aaeb14715c3c31af7424836cca066da74a
unknown <byuvaraj979@gmail.com>
2025-03-06 17:49:21 +0530

    6 Mar first commit Lab001

---
+++ src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basic;
+
+import java.util.Scanner;
+
+public class Lab001_HelloWorldProgram {
+    public static void main(String[]args){
+        String a;
+        a="Hello world";
+        System.out.println(a);
+    }
+}
