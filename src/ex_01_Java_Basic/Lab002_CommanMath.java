5c4d3925e6ce7a2891a363671755e203a1b3a457
Yuvaraj-B0 <byuvaraj979@gmail.com>
2025-03-10 18:03:44 +0530

    Mar 10 JavaComments&Math

---
+++ src/ex_01_Java_Basic/Lab002_CommanMath.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basic;
+
+public class Lab002_CommanMath {
+    public static void main(String [] args)
+    {
+        System.out.println("Addition of number 2+2 is : "  + (2+2));
+        System.out.println("Multiplication of number 11*11:"+(11*11));
+        System.out.println("Divison of two number 120/10:"+(120/10));
+        System.out.println("Substraction of number is 100-10:" +(100-10));
+        System.out.println("Modules of number is 100/10:" + (100/10));
+        System.out.println("Modules of number is 5/2:" + (5/2));
+    }
+}
---
+++ src/ex_01_Java_Basic/Lab002_Comments.java
@@ -0,0 +1,17 @@
+package ex_01_Java_Basic;
+
+public class Lab002_Comments {
+    public static void main (String[] args)
+    {
+        // Single-line Comments
+        // This is a comment
+        System.out.println("Hello World");
+
+        /* Multi-line Comments
+         */
+        /* The code below will print the words Hello World
+            to the screen, and it is amazing */
+        System.out.println("Hello World and Multi-line Comments");
+
+    }
+}
