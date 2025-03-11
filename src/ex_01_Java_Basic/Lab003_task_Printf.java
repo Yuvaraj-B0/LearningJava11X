4deb22702787f3361e6561184d5cd20bc3ad9024
Yuvaraj-B0 <byuvaraj979@gmail.com>
2025-03-11 08:19:38 +0530

    Mar 11 TaskToPrintTable9

---
+++ src/ex_01_Java_Basic/Lab003_task_Printf.java
@@ -0,0 +1,27 @@
+package ex_01_Java_Basic;
+
+public class Lab003_task_Printf {
+    public static void main(String[] args) {
+        int table = 9;
+        //this the task of today to print 9 table using printf
+        System.out.println("This done by using just printf");
+        System.out.printf("Table of 9X1 = %d\n",table);
+        System.out.printf("Table of 9X2 = %d\n",table*2);
+        System.out.printf("Table of 9X3 = %d\n",table*3);
+        System.out.printf("Table of 9X4 = %d\n",table*4);
+        System.out.printf("Table of 9X5 = %d\n",table*5);
+        System.out.printf("Table of 9X6 = %d\n",table*6);
+        System.out.printf("Table of 9X7 = %d\n",table*7);
+        System.out.printf("Table of 9X8 = %d\n",table*8);
+        System.out.printf("Table of 9X9 = %d\n",table*9);
+        System.out.printf("Table of 9X10 = %d\n",table*10);
+        //this the task of today to print 9 table using printf and using for loop
+        System.out.println("\nThis done by using for loop for table 9");
+        for(int a=1;a<=10;a++)
+        {
+            int temp=table*a;
+            System.out.printf("Table of 9X%d = %d\n",a,temp);
+        }
+
+    }
+}
