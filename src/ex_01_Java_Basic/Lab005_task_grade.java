b2a9e34f0323ed3f58c1389d11053eda16ccc9c8
Yuvaraj-B0 <byuvaraj979@gmail.com>
2025-03-13 09:05:34 +0530

    Mar 13 Task to Solve Grading system using nested ternary operator

---
+++ src/ex_01_Java_Basic/Lab005_task_grade.java
@@ -0,0 +1,21 @@
+package ex_01_Java_Basic;
+
+
+import java.util.Scanner;
+
+public class Lab005_task_grade {
+    public static void main(String[] args) {
+        int st_score;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter your score:");
+        st_score=sc.nextInt();
+        // Now Grading system works with nested ternary operator
+        String st_grade = (st_score<=59)?"Your Grade for scoring "+st_score+" is F"
+                :(st_score<=69)?"Your Grade for scoring "+st_score+" is D"
+                :(st_score<=79)?"Your Grade for scoring "+st_score+" is C"
+                :(st_score<=89)?"Your Grade for scoring "+st_score+" is B"
+                :(st_score<=100)?"Your Grade for scoring "+st_score+" is A"
+                :"Your have enter score is "+st_score+" it's beyond 100 Please Enter Correct Score under 100";
+        System.out.println(st_grade);
+    }
+}
