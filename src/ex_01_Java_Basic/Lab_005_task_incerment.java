695ff47bb08ded15c8dec84c8e7213d870bc912f
Yuvaraj-B0 <byuvaraj979@gmail.com>
2025-03-14 08:36:14 +0530

    Mar 14 AllTask

--- src/ex_01_Java_Basic/Lab_005_task_incerment.java
+++ src/ex_01_Java_Basic/Lab_005_task_incerment.java
@@ -6,6 +6,11 @@ public class Lab_005_task_incerment {
     public static void main(String[] args) {
         int a=10;
         System.out.println(++a + a++ + a++); //output is 34
+        /* ++a -> ex_a = 11 and a value 11
+           a++ -> ex_b = 11 and a value 12
+           a++ -> ex_c = 12 and a value 13
+           ex_a + ex_b + ex_c =34
+         */
         System.out.println(a); //out put 13
 
     }
--- src/ex_01_Java_Basic/Lab_006_task_decerment.java
+++ src/ex_01_Java_Basic/Lab_006_task_decerment.java
@@ -4,6 +4,12 @@ public class Lab_006_task_decerment {
     public static void main(String[] args) {
         int a=20;
         System.out.println(--a + a++ + a--); //output 58
+        /*
+        --a -> ex_a = 19 and a value is 19
+        a++ -> ex_b = 19 and a value is 20
+        a-- -> ex_c = 20 and a value is 19
+        ex_a + ex_b + ex_c = 58
+         */
         System.out.println(a); //output 19
     }
 }
