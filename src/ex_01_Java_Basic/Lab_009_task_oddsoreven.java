c307b57cac3777b8a4a11d3a6c837eb2d44aa9c3
Yuvaraj-B0 <byuvaraj979@gmail.com>
2025-03-14 08:43:24 +0530

    Mar 14 AllTask

---
+++ src/ex_01_Java_Basic/Lab_009_task_oddsoreven.java
@@ -0,0 +1,19 @@
+package ex_01_Java_Basic;
+
+import java.util.Scanner;
+
+public class Lab_009_task_oddsoreven {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number to check odd or even: ");
+        int Num=sc.nextInt();
+        if (Num%2==0)
+        {
+            System.out.println("The number " + Num + " is Even.");
+            //out put The number 2 is Even.
+        }
+        else {
+            System.out.println("The number " + Num + " is odds.");
+        }
+    }
+}
