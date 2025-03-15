10de26802531f27dfc4ed2f811676a10bfdcd0cd
Yuvaraj-B0 <byuvaraj979@gmail.com>
2025-03-15 18:52:26 +0530

    Mar 14 AllTask

--- src/Java_tasks/Lab_010_task_Triangle_Classifier.java
+++ src/Java_tasks/Lab_010_task_Triangle_Classifier.java
@@ -19,12 +19,16 @@ public class Lab_010_task_Triangle_Classifier {
         System.out.println("Enter the length of side 3: ");
         int side3=sc.nextInt();
         // Check if input forms a valid triangle
-        if (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1) {
-            if (side1 == side2 && side2 == side3) {
+        if (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1){
+            /*This uses the Triangle Inequality Theorem, which states that for three sides to form a valid triangle:
+              The sum of any two sides must be greater than the third side.
+              If this condition fails, the given side lengths do not form a triangle.*/
+            if (side1 == side2 && side2 == side3){ /*If all three sides are equal, it's an Equilateral Triangle.*/
                 System.out.println("The triangle is Equilateral.");
-            } else if (side1 == side2 || side2 == side3 || side1 == side3) {
+            } else if (side1 == side2 || side2 == side3 || side1 == side3){
+                /*If only two sides are equal, it's an Isosceles Triangle.*/
                 System.out.println("The triangle is Isosceles.");
-            } else {
+            } else { /*If all sides are different, it's a Scalene Triangle.*/
                 System.out.println("The triangle is Scalene.");
             }
         } else {
