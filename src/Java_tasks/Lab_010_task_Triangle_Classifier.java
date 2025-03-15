a3064d21376367ab89bac624eb0d78423f21278f
Yuvaraj-B0 <byuvaraj979@gmail.com>
2025-03-15 18:46:15 +0530

    Mar 14 AllTask

--- src/Java_tasks/Lab_010_task_Triangle_Classifier.java
+++ src/Java_tasks/Lab_010_task_Triangle_Classifier.java
@@ -4,6 +4,13 @@ import java.util.Scanner;
 
 public class Lab_010_task_Triangle_Classifier {
     public static void main(String[] args) {
+        /*
+         write me program in java Write a program that classifies a triangle based on its side lengths.
+         Given three input values representing the lengths of the sides,
+         determine if the triangle is equilateral (all sides are equal),
+         isosceles (exactly two sides are equal), or scalene (no sides are equal).
+          Use an if-else statement to classify the triangle.
+         */
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the length of side 1: ");
         int side1=sc.nextInt();
