c5e194b0163fd22abd34f9f1a8e7f469564735de
Yuvaraj-B0 <byuvaraj979@gmail.com>
2025-04-12 21:34:36 +0530

    APR 9 AllTask

---
+++ src/ex_Wrapper/Lab_Wrapper_Conversion.java
@@ -0,0 +1,17 @@
+package ex_Wrapper;
+
+public class Lab_Wrapper_Conversion {
+    public static void main(String[] args) {
+        int a= 10;
+        Integer b = a;  // This Boxing -> automatically JVM will store the value
+        // primitive to wrapper -> AutoBoxing
+        System.out.println(b.intValue()); // Behav
+        System.out.println(Integer.MIN_VALUE); // Behav
+
+        Integer a2 = 42;
+        int v = a2; // Unboxing -> wrapper is removed, attribute, behaviour is lost.
+        System.out.println(v);
+
+
+    }
+}
