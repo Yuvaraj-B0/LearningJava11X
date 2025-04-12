c62aa36328519b57674bae470838335f4e833739
Yuvaraj-B0 <byuvaraj979@gmail.com>
2025-04-12 21:43:49 +0530

    APR 9 AllTask

---
+++ src/ex_Execption/Lab_exceptions_sample.java
@@ -0,0 +1,12 @@
+package ex_Execption;
+
+public class Lab_exceptions_sample {
+    public static void main(String[] args) {
+        String name= null;
+        name.trim(); // java.lang.NullPointerException
+
+        // Checked Exception
+       // FileInputStream fileInputStream = new FileInputStream("C://testdata.txt");
+
+    }
+}
